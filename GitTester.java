0e24dd1cedf934ed4ff47545c33a68bea6dbfbf7
Talia <thannani1@hwemail.com>
2025-09-26 14:36:55 -0700

    (GP-2.1.1): Repository Initialization Testing
    Adds a tester class to verify the creation of the Git repository.
    The tester class initializes the repo, verifies the creation of the
    necessary directories and files, and cleans up the created files. This
    process is repeated multiple times to ensure the robustness of the
    initialization method.

---
+++ GitTester.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.io.IOException;
+
+public class GitTester {
+    public static void main(String[] args) throws IOException {
+        Git.initializeRepo();
+        verifyRepo();
+        File git = new File("git");
+        cleanUp(git);
+
+        // ensuring robust functionality
+        for (int i = 0; i < 300; i++) {
+            Git.initializeRepo();
+            verifyRepo();
+            cleanUp(git);
+        }
+
+    }
+    
+    public static void verifyRepo() {
+        File git = new File("git");
+        File objects = new File(git, "objects");
+        File index = new File(git, "index");
+        File head = new File(git, "HEAD");
+
+        if (git.exists() && objects.exists() && index.exists() && head.exists() && git.isDirectory() && objects.isDirectory() && index.isFile() && head.isFile()) {
+            System.out.println("All files and directories exist.");
+            return;
+        }
+
+        // if not all files/directories exist, see which one doesn't and make sure they are correctly files or directories
+
+        if (git.exists() && git.isDirectory()) {
+            System.out.println("Git directory exists");
+        } else {
+            System.out.println("Failed to make git directory");
+        }
+
+        if (objects.exists() && objects.isDirectory()) {
+            System.out.println("Objects directory exists");
+        } else {
+            System.out.println("Failed to make objects directory");
+        }
+
+        if (index.exists() && index.isFile()) {
+            System.out.println("Index file exists");
+        } else {
+            System.out.println("Failed to make index file");
+        }
+
+        if (head.exists() && head.isFile()) {
+            System.out.println("HEAD file exists");
+        } else {
+            System.out.println("Failed to make head file");
+        }
+    }
+
+
+    // https://www.geeksforgeeks.org/java/java-program-to-delete-a-directory/  
+    // https://stackoverflow.com/questions/20281835/how-to-delete-a-folder-with-files-using-java
+    // got help here for deleting directories
+    public static void cleanUp(File directory) {
+        for (File subFile : directory.listFiles()) {
+            if (subFile.isDirectory()) {
+                cleanUp(subFile);
+            }
+            subFile.delete();
+        }
+        directory.delete();
+    }
+
+
+    
+}
