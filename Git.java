d0ef43dee4874d8b8b96d000db4ba8e496929af2
Talia <thannani1@hwemail.com>
2025-09-26 10:09:18 -0700

    (GP-2.1): Program Can Initialize Repository
    Creates the basic directory structure and files
    required for a Git repository, including 'git',
    'git/objects', 'git/index', and 'git/HEAD'.
    
    Adds a check to see if repository has already been created.

--- Git.java
+++ Git.java
@@ -1,6 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Git {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        initializeRepo();
         
+    }
+
+    public static void initializeRepo() throws IOException{
+        File git = new File("git");
+        File objects = new File(git, "objects");
+        File index = new File(git, "index");
+        File head = new File(git, "HEAD");
+        if (objects.exists() && index.exists() && head.exists() && git.exists()) {
+            System.out.println("Git Repository Already Exists");
+            return;
+            }
+
+        if (!git.exists()) {
+            git.mkdir();}
+
+        if (!objects.exists()) {
+             objects.mkdir();
+        }
+
+        if (!index.exists()) {
+            index.createNewFile();
+        }
+        if (!head.exists()) {
+            head.createNewFile();
+        }
+
+        System.out.println("Git Repository Created");
+        return;   
     }
     
 }
